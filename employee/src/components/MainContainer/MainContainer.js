0d9fdc559a1dfddf0547403b8edadb4e0b12585c
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2021-04-18 19:37:58 +1000

    search function and clear function updating

--- employee/src/components/MainContainer/MainContainer.js
+++ employee/src/components/MainContainer/MainContainer.js
@@ -16,17 +16,19 @@ export default function MainContainer() {
 
 
     const handleInputChange = event => {
-        this.setSearch(event.target.value)
+        setSearch(event.target.value)
     };
 
     const handleFormSubmit = event => {
         event.preventDefault();
-        this.setEmployees(employees.filter(employee => search.indexOf(employee.name.first)> -1 || search.indexOf(employee.name.last)> -1))
+        setEmployees(employees.filter(employee => search.indexOf(employee.name.first)> -1 || search.indexOf(employee.name.last)> -1))
     };
 
     const clearSearch = event => {
         setSearch("");
         getEmployees()
+        .then(res => setEmployees(res.data.results))
+        .catch(err => console.log(err))
     };
 
         return (
@@ -34,7 +36,7 @@ export default function MainContainer() {
             {/* search function */}
             <div className="input-group mb-3 float-center">
                 <input value={search} onChange={handleInputChange} type="text" className="form-control" placeholder="Search Employee by Name" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-                <button className="btn btn-outline-danger" onClick={clearSearch}><i class="fas fa-window-close"></i></button>
+                <button className="btn btn-outline-danger" onClick={clearSearch}><i className="fas fa-window-close"></i></button>
                 <button onClick={handleFormSubmit} className="btn btn-outline-primary" type="submit" id="button-addon2">Search</button>
             </div>
             <div style={{display: "flex", justifyContent: "center",  margin: "auto", color: "gray"}}>
