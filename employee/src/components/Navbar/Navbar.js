d8ab627e98d60e2112240fc817e1968ad3c6068f
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2021-04-19 14:49:04 +1000

    styling nav and footer

--- employee/src/components/Navbar/Navbar.js
+++ employee/src/components/Navbar/Navbar.js
@@ -1,17 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./style.css"
 
 function Navbar(){
-return(
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
-          Employee Directory
-        </Link>
-      </li>
-    </ul>
-)
+  return (
+      <nav>
+          <h1 className="nav-title">Employee Directory</h1>
+      </nav>
+  )
 }
 
 export default Navbar;
